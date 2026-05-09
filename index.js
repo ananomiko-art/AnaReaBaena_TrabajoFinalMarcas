c4c63ba4d5c69093879bf1d380e091fea1ec656f
ananomiko-art <ananomiko@gmail.com>
2026-05-09 14:15:48 +0200

    Añadido Recurso secundario y Comentarios
    He añadido el recurso secundario que serán los personajes y están conectados a la principal (Hechizos) por medio del id del hechizo según cual sea su especialidad.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 1987;
+const port = 1987; /*Puerto de la api*/
 
 app.use(express.json())
 
 app.listen(port, () => 
-console.log("Alohomora.."))
+console.log("Alohomora..")) /*Mesnaje de apertura*/
 
 let Hechizos = [
     {id: 1, nombre:'Accio', tipo: 'Utilidad', dificultad: 'Baja', año_aprend:'1º', coste:1, dado:'d6', stat_calc:'Habilidad', descripción:'Atrae el objeto o persona afectado' },
+    {id: 2, nombre:'Levioso', tipo: 'Utilidad', dificultad: 'Baja', año_aprend:'1º', coste:1, dado:'d6', stat_calc:'Habilidad', descripción:'La persona u objeto es levantada en el aire'},
+]
 
+let Personajes = [
+    {id: 1, nombre: 'Natasha Dreamoor', casa: 'Hufflepuff', año: '1º', huecos: 3, IdHechizoPrincipal: 1},
+    {id: 2, nombre: 'Henry Runewood', casa: 'Ravenclaw', año: '1º', huecos: 4, IdHechizoPrincipal: 2}
 ]
