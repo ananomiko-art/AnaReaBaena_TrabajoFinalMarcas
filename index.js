cd923654436cc74975bf0ca476dda87403549535
ananomiko-art <ananomiko@gmail.com>
2026-05-10 12:03:00 +0200

    añadido operaciones basicas y ordenado el código
    He terminado el apartado de operaciones básicas y he reordenado el código para que sea más entendible. También he empezado parte del siguiente punto.

--- index.js
+++ index.js
@@ -17,14 +17,69 @@ let Personajes = [
     {id: 2, nombre: 'Henry Runewood', casa: 'Ravenclaw', año: '1º', huecos: 4, IdHechizoPrincipal: 2}
 ]
 
+/*==============operaciones basicas de personajes=====================*/
+
+//seleccionar solo los personajes
 
 app.get("/personajes", (req, res)=>{ /*Si es barra personajes solo salen los personajes*/
     return res.json(Personajes)
 })
 
+//buscar x id
+
+app.get("/personajes/:id", (req, res)=>{ //Esto es para poder especificar la id que buscas del personje
+    return res.json(Personajes[req.params.id - 1])
+})
+
+//buscar x nombre
+
+app.get("/personaje", (req, res) => {//Busca el personaje por nombre
+    let nombre = req.query.nombre
+    let resultado = Personajes.find(p => p.nombre == nombre)
+    return res.json(resultado)
+})
+
+//Eliminar
+
+app.delete("/personajes/:id", (req, res) => { //Busco el registro por la id
+    let i = req.params.id - 1 //guardo la id
+    let eliminado = Personajes.splice(i, 1) //y hago una variable que indique que elimine el registro con id de i
+    return res.json(eliminado) //y responde a la api que lo ha eliminado
+})
+
+/*==================================================================*/
+
+/*==============operaciones basicas de hechizos=====================*/
+
+//seleccionar solo los hechizos
+
 app.get("/hechizos", (req, res)=>{ /*Igual que personajes pero con hechizos*/
     return res.json(Hechizos)
 })
+
+//buscar x id
+
+app.get("/hechizos/:id", (req, res)=>{ //Y esto para la id del hechizo
+    return res.json(Hechizos[req.params.id - 1])
+})
+
+//Buscar x nombre
+
+app.get("/hechizo", (req, res) => {//Busca el hechizo por nombre
+    let nombre = req.query.nombre
+    let resultado = Hechizos.find(h => h.nombre == nombre)
+    return res.json(resultado)
+})
+
+//Eliminar
+
+app.delete("/hechizos/:id", (req, res) => { //Busco el registro por la id
+    let i = req.params.id - 1 //guardo la id
+    let eliminado = Hechizos.splice(i, 1) //y hago una variable que indique que elimine el registro con id de i
+    return res.json(eliminado) //y responde a la api que lo ha eliminado
+})
+
+/*==================================================================*/
 
 app.get("/", (req, res) => { /*Si simplemente pones barra sale todo*/
 
@@ -35,25 +90,7 @@ app.get("/", (req, res) => { /*Si simplemente pones barra sale todo*/
 
 })
 
-app.get("/personajes/:id", (req, res)=>{ //Esto es para poder especificar la id que buscas del personje
-    return res.json(Personajes[req.params.id - 1])
-})
-
-app.get("/hechizos/:id", (req, res)=>{ //Y esto para la id del hechizo
-    return res.json(Hechizos[req.params.id - 1])
-})
-
-app.get("/hechizo", (req, res) => {//Busca el hechizo por nombre
-    let nombre = req.query.nombre
-    let resultado = Hechizos.find(h => h.nombre == nombre)
-    return res.json(resultado)
-})
-
-app.get("/personaje", (req, res) => {//Busca el hechizo por nombre
-    let nombre = req.query.nombre
-    let resultado = Personajes.find(p => p.nombre == nombre)
-    return res.json(resultado)
-})
+/*======================Añadir hechizo=======================*/
 
 app.post("/añadir-hechizo", (req, res)=>{ //Para añadir hechizos
 
@@ -73,9 +110,36 @@ app.post("/añadir-hechizo", (req, res)=>{ //Para añadir hechizos
         descripción: req.body.descripción
     }
 
-    personajes.push(HechizoNuevo); //Esto indica que añada el hehcizo
-    return res.status.json(HechizoNuevo)
+    personajes.push(nuevoHechizo); //Esto indica que añada el hehcizo
+    return res.status(201).json(nuevoHechizo)
 })
+
+/*=====================================================================*/
+
+/*======================Añadir personaje=======================*/
+
+app.post("/añadir-personajes", (req, res) => { //Aquí está para añadir personajes
+
+    if(!req.body.nombre || !req.body.casa){ //Verifica los campos
+        return res.status(400).json({mensaje: "Faltan datos"}) //Muestra el mensaje si falta algo
+    }
+
+    let nuevoPersonaje = { //indico los parametros
+        id: Personajes.length + 1,
+        nombre: req.body.nombre,
+        casa: req.body.casa,
+        año: req.body.año,
+        huecos: req.body.huecos,
+        IdHechizoPrincipal: req.body.IdHechizoPrincipal
+    }
+
+    Personajes.push(nuevoPersonaje) //pusheo el personaje
+    res.status(201).json(nuevoPersonaje)
+})
+
+/*=====================================================================*/
+
+/*======================Modificar hechizo=======================*/
 
 app.put("/hechizos/:id", (req, res) => { //Esto modifica un registro que ya existe
 
@@ -88,8 +152,39 @@ app.put("/hechizos/:id", (req, res) => { //Esto modifica un registro que ya exis
     return res.json(Hechizos[i]) //y los devuelve como respuesta a la api
 })
 
-app.delete("/hechizos/:id", (req, res) => { //Busco el registro por la id
-    let i = req.params.id - 1 //guardo la id
-    let eliminado = Hechizos.splice(i, 1) //y hago una variable que indique que elimine el registro con id de i
-    return res.json(eliminado) //y responde a la api que lo ha eliminado
+/*=====================================================================*/
+
+/*======================Modificar personajes=======================*/
+
+app.put("/personajes/:id", (req, res) => { //Esto modifica un registro que ya existe
+
+    let i = req.params.id - 1 //guardo el id del que quieras modificar
+
+    Personajes[i].nombre = req.body.nombre //pide los parametros del parametro que indique i
+    Personajes[i].tipo = req.body.tipo
+    Personajes[i].dificultad = req.body.dificultad
+
+    return res.json(Personajes[i]) //y los devuelve como respuesta a la api
 })
+
+/*=====================================================================*/
+
+/*==============Obtener los hechizos de un personaje en concreto=====================*/
+
+app.get("/personajes/:id/hechizos", (req, res) => {
+
+   let id = int(req.params.id) 
+
+    let personaje = Personajes.find(p => p.id === id) //Uso .find para buscar el perosnaje, es más util que lo que estaba usando(busca en un array)
+
+    if(!personaje){
+        return res.status(404).json({mensaje: "Personaje no encontrado"})//si no encuentra nada envio el mensaje, o sea error 404
+    }
+
+    let hechizo = Hechizos.find(h => h.id === personaje.IdHechizoPrincipal) //Busco la id del hechizo que sea igual al del hechizo principal del personaje
+
+    res.json(hechizo) //respuesta
+
+})
+
+/*=====================================================================*/
